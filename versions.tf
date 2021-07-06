838293718e201ab8fc3e176025dd90d39bd1e6b5
awright111 <50182961+awright111@users.noreply.github.com>
2021-07-06 14:47:46 -0500

    comment out AWS Version

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.20.0"
-    }
+    # aws = {
+    #   source  = "hashicorp/aws"
+    #   version = ">= 3.20.0"
+    # }
 
     random = {
       source  = "hashicorp/random"
